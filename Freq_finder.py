42405c6fe7f03ed5339472c317bda667cf6d854c
Parth <kattiparth1.6@gmail.com>
2026-02-25 00:55:44 +0530

    Added a code for finding specific frequency for TAI onset

---
+++ Freq_finder.py
@@ -0,0 +1,54 @@
+# imports 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import welch
+
+# varaibles
+Freq = 44100
+sampling_freq = 1/Freq
+
+
+# declaring a dict for reynolds number
+re_map = {
+    15: 1565.85, 
+    20: 2087.80, 
+    25: 2609.75, 
+    30: 3131.70,
+    35: 3653.65, 
+    40: 4175.60, 
+    43: 4488.77, 
+    45: 4697.55,
+    47: 4906.33, 
+    50: 5219.50, 
+    53: 5532.67
+}
+
+# now we loop thru every number in the 
+# given reynold's number dict
+
+for file_num, reyn in re_map.items():
+    file_name = str(file_num) + ".xlsx"
+
+    # now we open the file using pandas
+    df1 = pd.read_excel(file_name, header=None, names=['Amplitude']) # defining the data frame
+
+    # now we extract the signal from the dataframe
+    signal = df1['Amplitude'].to_numpy()
+
+    # now we calculate the frequency spectrum 
+    # nperseg = 4096 gives highest resolution
+    freqs, psd = welch(signal, fs=Freq, nperseg=4096)
+
+    # now we plot it using log scale.
+    plt.semilogy(freqs, psd, label=f"Re: {reyn}")
+
+# formatting the graph
+plt.title("Frequency Spectrum Waterfall")
+plt.xlabel("Frequency (Hz)")
+plt.ylabel("Power Spectral Density")
+plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left') # moves legend outside to keep graph clean
+plt.xlim(0, 500) # zoom in on the low frequencies where TAI lives
+plt.grid(True, which="both", ls="-", alpha=0.5)
+plt.tight_layout()
+plt.show()
